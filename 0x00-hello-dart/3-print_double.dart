9c773880053913e967995a537e3189ebe8bc530c
MbongX <Mbongenimahlangu203@outlook.com>
2026-05-19 10:26:28 +0200

    Resubmission after evaluation: - Removed Decimal package (as server could not resolve this) - Sticking to simple implementation using toStringAsFixed

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,13 +1,11 @@
-import 'package:decimal/decimal.dart';
-import 'dart:math';
 
 void main(List<String> arguments){
   double piValue = 3.1415926;
   // Convert double pi -> string -> Decimal to avoid direct fromDouble errors  
-  final Decimal piDecimal = Decimal.parse(piValue.toString());
+  //final Decimal piDecimal = Decimal.parse(piValue.toString());
   
   // Round to 2 decimal places
-  final roundedPi = Decimal.parse(piDecimal.toStringAsFixed(2));
+  final roundedPi = piValue.toStringAsFixed(2);
   
   print("Double: ${roundedPi}\n");
 }
