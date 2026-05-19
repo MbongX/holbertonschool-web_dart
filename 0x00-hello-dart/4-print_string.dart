e1f1db91e63a1dd3b339835969b35de98ec9eaf2
MbongX <Mbongenimahlangu203@outlook.com>
2026-05-19 12:08:28 +0200

    Resubmission(3) of Task 4
    - updated program to use a tenary operator to check if there are any args passed in, if not use the default string value

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,5 @@
 void main(List<String> arguments){
-  String str = "Holberton School";
+  String str = (arguments.isEmpty || arguments[0].isEmpty) ?  "Holberton School" : arguments[0];
   print(str * 3 + "\n");
   print(str.substring(0,9));
 }
