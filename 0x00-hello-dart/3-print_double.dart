e5567e878a6ff939ee84f2ac92df62601a21b89b
MbongX <Mbongenimahlangu203@outlook.com>
2026-05-19 10:30:59 +0200

    Resubmission (2) after evaluation: - updated variable name to number from piValue -

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,11 +1,11 @@
 
 void main(List<String> arguments){
-  double piValue = 3.1415926;
+  double number = 3.1415926;
   // Convert double pi -> string -> Decimal to avoid direct fromDouble errors  
   //final Decimal piDecimal = Decimal.parse(piValue.toString());
   
   // Round to 2 decimal places
-  final roundedPi = piValue.toStringAsFixed(2);
+  final roundedPi = number.toStringAsFixed(2);
   
   print("Double: ${roundedPi}\n");
 }
