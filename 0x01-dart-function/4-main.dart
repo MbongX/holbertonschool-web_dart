c4d055344700d6b69d319598e16bf7546156ecde
MbongX <Mbongenimahlangu203@outlook.com>
2026-05-28 11:38:25 +0200

    Dart Project 2 : Dart - Function [Resubmission]
    Task 3 : Outer Inner Function Program
    
    * Resubmission of Task 4 - Outer Inner function program.
    * Removal of Class Implementation and static accessors
    * Updated Readme with Task 4
    
    Signed-off-by: MbongX <Mbongenimahlangu203@outlook.com>

--- 0x01-dart-function/4-main.dart
+++ 0x01-dart-function/4-main.dart
@@ -1,5 +1,5 @@
 import '4-outer_inner.dart';
 
 void main(List<String> arguments){
-  outer_inner.outer("Youssef Belhadj", "001");
+  outer("Youssef Belhadj", "001");
 }
--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,21 +1,18 @@
-class outer_inner{
-  static late String finalName;
-  static late String finalId;
+late String finalName;
+late String finalId;
   
-  static void outer(String name, String id){
-    // format the name here
-    String initials = name.split(" ").map((word) => word[0]).join("");
-    initials = initials[1];
-    String lastName = name.split(" ").first;
-    finalName = initials + "." + lastName;
-    finalId = id;
-    
-    print(inner());
-  }
-  
-  static String inner(){
-    //
-    return ("Hello Agent $finalName, your ID is $finalId");
-  }
-  
+void outer(String name, String id){
+  // format the name here
+  String initials = name.split(" ").map((word) => word[0]).join("");
+  String lastName = name.split(" ").first;
+  initials = initials[1];
+  finalName = initials + "." + lastName;
+  finalId = id;
+  //calling the inner function
+  print(inner());
 }
+  
+String inner(){
+  return ("Hello Agent $finalName, your ID is $finalId");
+}
+  
